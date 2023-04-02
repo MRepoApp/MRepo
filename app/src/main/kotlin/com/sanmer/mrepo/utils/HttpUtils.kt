a23326d3c8ce59252282262c6f070850c0dc7d94
ya0211 <sanmer@sanmer.dev>
2023-04-02 14:12:59 +0800

    Refactor HttpUtils

--- app/src/main/kotlin/com/sanmer/mrepo/utils/HttpUtils.kt
+++ app/src/main/kotlin/com/sanmer/mrepo/utils/HttpUtils.kt
@@ -2,6 +2,8 @@ package com.sanmer.mrepo.utils
 
 import com.sanmer.mrepo.utils.MediaStoreUtils.newOutputStream
 import com.sanmer.mrepo.utils.expansion.runRequest
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.adapter
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import okhttp3.OkHttpClient
@@ -30,44 +32,45 @@ object HttpUtils {
         get = { it.string() }
     )
 
+    suspend inline fun <reified T> requestJson(
+        url: String
+    ): Result<T> = request(url) {
+        val adapter = Moshi.Builder()
+            .build()
+            .adapter<T>()
+
+        return@request adapter.fromJson(it.string())!!
+    }
+
     suspend fun downloader(
         url: String,
         out: File,
         onProgress: (Float) -> Unit
-    ): Result<File> {
+    ): Result<File> = request(url) { body ->
+        val buffer = ByteArray(2048)
+        val input = body.byteStream()
+
         out.parentFile!!.let {
-            if (!it.exists())
-                it.mkdirs()
+            if (!it.exists()) it.mkdirs()
+        }
+        val output = out.newOutputStream()
+
+        val all = body.contentLength()
+        var finished: Long = 0
+        var readying: Int
+
+        while (input.read(buffer).also { readying = it } != -1) {
+            output?.write(buffer, 0, readying)
+            finished += readying.toLong()
+
+            val progress = (finished * 1.0 / all).toFloat()
+            onProgress(progress)
         }
 
-        val get: (ResponseBody) -> File = { body ->
-            val buffer = ByteArray(2048)
-            val input = body.byteStream()
+        output?.flush()
+        output?.close()
+        input.close()
 
-            val output = out.newOutputStream()
-
-            val all = body.contentLength()
-            var finished: Long = 0
-            var readying: Int
-
-            while (input.read(buffer).also { readying = it } != -1) {
-                output?.write(buffer, 0, readying)
-                finished += readying.toLong()
-
-                val progress = (finished * 1.0 / all).toFloat()
-                onProgress(progress)
-            }
-
-            output?.flush()
-            output?.close()
-            input.close()
-
-            out
-        }
-
-        return request(
-            url = url,
-            get = get
-        )
+        return@request out
     }
 }
