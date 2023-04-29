33195498495d1ce6a98b19729796916cd1056f08
ya0211 <sanmer@sanmer.dev>
2023-04-29 14:23:56 +0800

    Remove SetupActivity

--- app/src/main/kotlin/com/sanmer/mrepo/ui/activity/main/MainActivity.kt
+++ app/src/main/kotlin/com/sanmer/mrepo/ui/activity/main/MainActivity.kt
@@ -2,33 +2,71 @@ package com.sanmer.mrepo.ui.activity.main
 
 import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.ViewTreeObserver
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import com.sanmer.mrepo.datastore.WorkingMode
 import com.sanmer.mrepo.ui.activity.base.BaseActivity
-import com.sanmer.mrepo.ui.activity.setup.SetupActivity
 import com.sanmer.mrepo.utils.NotificationUtils
+import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.launch
 
 class MainActivity : BaseActivity() {
+    private var isReady = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val content: View = findViewById(android.R.id.content)
+        content.viewTreeObserver.addOnPreDrawListener(
+            object : ViewTreeObserver.OnPreDrawListener {
+                override fun onPreDraw(): Boolean = if (isReady) {
+                    content.viewTreeObserver.removeOnPreDrawListener(this)
+                    true
+                } else {
+                    false
+                }
+            }
+        )
 
         lifecycleScope.launch {
             lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
                 userDataRepository.userData
+                    .distinctUntilChanged()
                     .collect {
-                        if (it.isSetup) SetupActivity.start(this@MainActivity)
+                        if (it.isSetup) {
+                            setSetup()
+                        } else {
+                            setMain()
+                        }
+                        isReady = true
                     }
             }
         }
+    }
 
-        setActivityContent {
-            if (!it.isSetup && Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-                NotificationUtils.PermissionState()
-            }
-
-            MainScreen()
+    private fun setSetup() = setActivityContent {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            NotificationUtils.PermissionState()
         }
+
+        SetupScreen(
+            onRoot = {
+                userDataRepository.setWorkingMode(WorkingMode.MODE_ROOT)
+            },
+            onNonRoot = {
+                userDataRepository.setWorkingMode(WorkingMode.MODE_NON_ROOT)
+            }
+        )
+    }
+
+    private fun setMain() = setActivityContent {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            NotificationUtils.PermissionState()
+        }
+
+        MainScreen()
     }
 }
--- app/src/main/kotlin/com/sanmer/mrepo/ui/activity/main/SetupScreen.kt
+++ app/src/main/kotlin/com/sanmer/mrepo/ui/activity/main/SetupScreen.kt
@@ -1,4 +1,4 @@
-package com.sanmer.mrepo.ui.activity.setup
+package com.sanmer.mrepo.ui.activity.main
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
