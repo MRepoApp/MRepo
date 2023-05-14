56fb3de1849dc78413a463c7327305d439de65a4
ya0211 <sanmer@sanmer.dev>
2023-05-14 23:41:17 +0800

    Remove unnecessary Shortcuts

--- app/src/main/kotlin/com/sanmer/mrepo/app/utils/ShortcutUtils.kt
+++ app/src/main/kotlin/com/sanmer/mrepo/app/utils/ShortcutUtils.kt
@@ -5,19 +5,18 @@ import androidx.core.content.pm.ShortcutInfoCompat
 import androidx.core.content.pm.ShortcutManagerCompat
 import androidx.core.graphics.drawable.IconCompat
 import com.sanmer.mrepo.App
-import com.sanmer.mrepo.BuildConfig
 import com.sanmer.mrepo.R
 import com.sanmer.mrepo.ui.activity.log.LogActivity
 
 object ShortcutUtils {
     private val context by lazy { App.context }
     private const val ID_LOGS = "logs"
-    private const val ID_SETTINGS = "settings"
-    private const val ID_MODULES = "modules"
-    const val ACTION_MODULES = "${BuildConfig.APPLICATION_ID}.shortcut.MODULES"
-    const val ACTION_SETTINGS = "${BuildConfig.APPLICATION_ID}.shortcut.SETTINGS"
 
-    val logs get() = run {
+    init {
+        ShortcutManagerCompat.removeAllDynamicShortcuts(context)
+    }
+
+    private val logs get() = run {
         val activity = Intent(Intent.ACTION_MAIN, null, context, LogActivity::class.java)
 
         ShortcutInfoCompat.Builder(context, ID_LOGS)
@@ -28,31 +27,7 @@ object ShortcutUtils {
             .build()
     }
 
-    val settings get() = run {
-        val page = Intent(ACTION_SETTINGS)
-
-        ShortcutInfoCompat.Builder(context, ID_SETTINGS)
-            .setShortLabel(context.getString(R.string.shortcut_settings_label))
-            .setLongLabel(context.getString(R.string.shortcut_settings_label))
-            .setIcon(IconCompat.createWithResource(context, R.drawable.shortcut_settings))
-            .setIntent(page)
-            .build()
-    }
-
-    val modules get() = run {
-        val page = Intent(ACTION_MODULES)
-
-        ShortcutInfoCompat.Builder(context, ID_MODULES)
-            .setShortLabel(context.getString(R.string.shortcut_modules_label))
-            .setLongLabel(context.getString(R.string.shortcut_modules_label))
-            .setIcon(IconCompat.createWithResource(context, R.drawable.shortcut_modules))
-            .setIntent(page)
-            .build()
-    }
-
     fun push() {
         ShortcutManagerCompat.pushDynamicShortcut(context, logs)
-        ShortcutManagerCompat.pushDynamicShortcut(context, settings)
-        ShortcutManagerCompat.pushDynamicShortcut(context, modules)
     }
 }
