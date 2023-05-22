a6ff57c35369a54a02e1cc0bbea0713a1e0bd337
ya0211 <sanmer@sanmer.dev>
2023-05-22 15:46:08 +0800

    Use IntentCompat and BundleCompat

--- app/src/main/kotlin/com/sanmer/mrepo/utils/expansion/Parcelable.kt
+++ app/src/main/kotlin/com/sanmer/mrepo/utils/expansion/Parcelable.kt
@@ -1,17 +1,15 @@
 package com.sanmer.mrepo.utils.expansion
 
 import android.content.Intent
-import android.os.Build.VERSION.SDK_INT
-import android.os.Build.VERSION_CODES.TIRAMISU
 import android.os.Bundle
 import android.os.Parcelable
+import androidx.core.content.IntentCompat
+import androidx.core.os.BundleCompat
 
-inline fun <reified T : Parcelable> Intent.parcelable(key: String): T? = when {
-    SDK_INT >= TIRAMISU -> getParcelableExtra(key, T::class.java)
-    else -> @Suppress("DEPRECATION") getParcelableExtra(key) as? T
-}
+inline fun <reified T : Parcelable> Intent.parcelable(
+    key: String
+): T? = IntentCompat.getParcelableExtra(this, key, T::class.java)
 
-inline fun <reified T : Parcelable> Bundle.parcelable(key: String): T? = when {
-    SDK_INT >= TIRAMISU -> getParcelable(key, T::class.java)
-    else -> @Suppress("DEPRECATION") getParcelable(key) as? T
-}
+inline fun <reified T : Parcelable> Bundle.parcelable(
+    key: String
+): T? = BundleCompat.getParcelable(this, key, T::class.java)
