fe17c9a9091254e6153f14b7a830f9988e83dcf2
ya0211 <sanmer@sanmer.dev>
2023-07-19 15:43:34 +0800

    Fix Checkbox

--- app/src/main/kotlin/com/sanmer/mrepo/ui/component/Checkbox.kt
+++ app/src/main/kotlin/com/sanmer/mrepo/ui/component/Checkbox.kt
@@ -1,18 +1,20 @@
 package com.sanmer.mrepo.ui.component
 
-import androidx.compose.animation.core.FastOutSlowInEasing
-import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.snap
+import androidx.compose.animation.core.spring
 import androidx.compose.animation.core.tween
+import androidx.compose.animation.core.updateTransition
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.selection.triStateToggleable
 import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.minimumInteractiveComponentSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
@@ -29,45 +31,87 @@ fun Checkbox(
     enabled: Boolean = true,
     interactionSource: MutableInteractionSource = remember { MutableInteractionSource() }
 ) {
-    val alpha by animateFloatAsState(
-        targetValue = if (checked) 1f else 0f,
-        animationSpec = tween(
-            durationMillis = 400,
-            easing = FastOutSlowInEasing
-        ),
-        label = "alpha"
+    TriStateCheckbox(
+        state = ToggleableState(checked),
+        onClick = if (onCheckedChange != null) {
+            { onCheckedChange(!checked) }
+        } else {
+            null
+        },
+        modifier = modifier,
+        enabled = enabled,
+        interactionSource = interactionSource
     )
+}
 
-    Box(modifier = modifier
-        .then(if (onCheckedChange != null) {
+@Composable
+fun TriStateCheckbox(
+    state: ToggleableState,
+    onClick: (() -> Unit)?,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() }
+) {
+    val toggleableModifier =
+        if (onClick != null) {
             Modifier.triStateToggleable(
-                state = ToggleableState(checked),
-                onClick = { onCheckedChange(!checked) },
+                state = state,
+                onClick = onClick,
                 enabled = enabled,
                 role = Role.Checkbox,
                 interactionSource = interactionSource,
                 indication = rememberRipple(
                     bounded = false,
-                    radius = 20.dp
+                    radius = 40.0.dp / 2 //CheckboxTokens.StateLayerSize / 2
                 )
             )
         } else {
             Modifier
-        }),
-        contentAlignment = Alignment.Center
+        }
+
+    val transition = updateTransition(state, label = "TriStateCheckbox")
+    val checkDrawFraction by transition.animateFloat(
+        transitionSpec = {
+            when {
+                initialState == ToggleableState.Off -> tween(CheckAnimationDuration)
+                targetState == ToggleableState.Off -> snap(BoxOutDuration)
+                else -> spring()
+            }
+        },
+        label = "checkDrawFraction"
+    ) {
+        when (it) {
+            ToggleableState.On -> 1f
+            ToggleableState.Off -> 0f
+            ToggleableState.Indeterminate -> 1f
+        }
+    }
+
+    Box(modifier = modifier
+        .then(
+            if (onClick != null) {
+                Modifier.minimumInteractiveComponentSize()
+            } else {
+                Modifier
+            }
+        )
+        .then(toggleableModifier)
     ) {
         Image(
             painter = painterResource(id = R.drawable.tick_square_bold),
             contentDescription = null,
-            alpha = alpha,
+            alpha = checkDrawFraction,
             colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.primary)
         )
 
         Image(
             painter = painterResource(id = R.drawable.square_outline),
             contentDescription = null,
-            alpha = 1f - alpha,
+            alpha = 1f - checkDrawFraction,
             colorFilter = ColorFilter.tint(MaterialTheme.colorScheme.onSurfaceVariant)
         )
     }
 }
+
+private const val BoxOutDuration = 100
+private const val CheckAnimationDuration = 100
