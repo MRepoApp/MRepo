1b2c3e3f01547c882e886b6dc661002231d43026
ya0211 <sanmer@sanmer.dev>
2023-08-06 20:54:11 +0800

    Tidy up runRequest

--- app/src/main/kotlin/com/sanmer/mrepo/utils/extensions/ResponseExtensions.kt
+++ app/src/main/kotlin/com/sanmer/mrepo/utils/extensions/ResponseExtensions.kt
@@ -1,5 +1,11 @@
 package com.sanmer.mrepo.utils.extensions
 
+fun isHTML(text: String): Boolean {
+    return "<html\\s*>|<head\\s*>|<body\\s*>|<!doctype\\s*html\\s*>"
+        .toRegex()
+        .containsMatchIn(text)
+}
+
 inline fun <reified T> runRequest(
     run: () -> retrofit2.Response<T>
 ): Result<T> = try {
@@ -9,39 +15,13 @@ inline fun <reified T> runRequest(
         if (data != null) {
             Result.success(data)
         }else {
-            Result.failure(NullPointerException("The data is null!"))
+            Result.failure(NullPointerException())
         }
     } else {
         val errorBody = response.errorBody()
         val error = errorBody?.string() ?: "404 Not Found"
 
-        if ("</html>" in error) {
-            Result.failure(RuntimeException("404 Not Found"))
-        } else {
-            Result.failure(RuntimeException(error))
-        }
-    }
-} catch (e: Exception) {
-    Result.failure(e)
-}
-
-inline fun <reified T> runRequest(
-    get: (okhttp3.ResponseBody) -> T,
-    run: () -> okhttp3.Response
-): Result<T> = try {
-    val response = run()
-    if (response.isSuccessful) {
-        val data = response.body()
-        if (data != null) {
-            Result.success(get(data))
-        } else {
-            Result.failure(NullPointerException("The data is null!"))
-        }
-    } else {
-        val errorBody = response.body()
-        val error = errorBody?.string() ?: "404 Not Found"
-
-        if ("</html>" in error) {
+        if (isHTML(error)) {
             Result.failure(RuntimeException("404 Not Found"))
         } else {
             Result.failure(RuntimeException(error))
@@ -61,13 +41,39 @@ inline fun <T, reified R> runRequest(
         if (data != null) {
             Result.success(convert(data))
         }else {
-            Result.failure(NullPointerException("The data is null!"))
+            Result.failure(NullPointerException())
         }
     } else {
         val errorBody = response.errorBody()
         val error = errorBody?.string() ?: "404 Not Found"
 
-        if ("</html>" in error) {
+        if (isHTML(error)) {
+            Result.failure(RuntimeException("404 Not Found"))
+        } else {
+            Result.failure(RuntimeException(error))
+        }
+    }
+} catch (e: Exception) {
+    Result.failure(e)
+}
+
+inline fun <reified T> runRequest(
+    get: (okhttp3.ResponseBody) -> T,
+    run: () -> okhttp3.Response
+): Result<T> = try {
+    val response = run()
+    if (response.isSuccessful) {
+        val data = response.body()
+        if (data != null) {
+            Result.success(get(data))
+        } else {
+            Result.failure(NullPointerException())
+        }
+    } else {
+        val errorBody = response.body()
+        val error = errorBody?.string() ?: "404 Not Found"
+
+        if (isHTML(error)) {
             Result.failure(RuntimeException("404 Not Found"))
         } else {
             Result.failure(RuntimeException(error))
