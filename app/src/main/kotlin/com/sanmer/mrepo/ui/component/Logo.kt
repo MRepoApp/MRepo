73439abb2694fe861cbe8960b15a65da6db4d610
ya0211 <sanmer@sanmer.dev>
2023-08-14 13:52:58 +0800

    Optimize Logo component

--- app/src/main/kotlin/com/sanmer/mrepo/ui/component/Logo.kt
+++ app/src/main/kotlin/com/sanmer/mrepo/ui/component/Logo.kt
@@ -1,39 +1,41 @@
 package com.sanmer.mrepo.ui.component
 
 import androidx.annotation.DrawableRes
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Icon
+import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
 
 @Composable
 fun Logo(
     @DrawableRes icon: Int,
     modifier: Modifier = Modifier,
+    shape: Shape = CircleShape,
     contentColor: Color = MaterialTheme.colorScheme.onPrimary,
     containerColor: Color = MaterialTheme.colorScheme.primary
-) = Box(
-    modifier = modifier
-        .clip(CircleShape)
-        .shadow(elevation = 10.dp)
-        .background(color = containerColor),
-    contentAlignment = Alignment.Center
+) = Surface(
+    modifier = modifier,
+    shape = shape,
+    color = containerColor,
+    contentColor = contentColor
 ) {
-    Icon(
-        modifier = Modifier
-            .fillMaxSize(0.6f),
-        painter = painterResource(id = icon),
-        contentDescription = null,
-        tint = contentColor
-    )
+    Box(
+        contentAlignment = Alignment.Center
+    ) {
+        Icon(
+            modifier = Modifier.fillMaxSize(0.6f),
+            painter = painterResource(id = icon),
+            contentDescription = null,
+            tint = LocalContentColor.current
+        )
+    }
 }
