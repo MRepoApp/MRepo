a59dd297cabacae35651fc82a5a7eb9713e9cb61
ya0211 <sanmer@sanmer.dev>
2023-08-20 17:27:47 +0800

    Fix State of Repo

--- app/src/main/kotlin/com/sanmer/mrepo/ui/screens/settings/repositories/RepositoriesList.kt
+++ app/src/main/kotlin/com/sanmer/mrepo/ui/screens/settings/repositories/RepositoriesList.kt
@@ -5,12 +5,13 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -35,11 +36,13 @@ fun RepositoriesList(
     contentPadding = PaddingValues(10.dp),
     verticalArrangement = Arrangement.spacedBy(10.dp),
 ) {
-    items(
+    itemsIndexed(
         items = list,
-        key = { it.url }
-    ) { repo ->
-        val value by remember(list) { mutableStateOf(repo) }
+        key = { _, repo ->
+            repo.url
+        }
+    ) { index, _ ->
+        val value by remember(list) { derivedStateOf { list[index] } }
 
         RepositoryItem(
             repo = value,
