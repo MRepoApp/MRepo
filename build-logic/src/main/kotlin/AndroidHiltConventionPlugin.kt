447fe6905bf439595f88dba6679646333d314186
ya0211 <sanmer@sanmer.dev>
2023-08-31 10:33:53 +0800

    Use KSP

--- build-logic/src/main/kotlin/AndroidHiltConventionPlugin.kt
+++ build-logic/src/main/kotlin/AndroidHiltConventionPlugin.kt
@@ -2,10 +2,8 @@
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.artifacts.VersionCatalogsExtension
-import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
 import org.gradle.kotlin.dsl.getByType
-import org.jetbrains.kotlin.gradle.plugin.KaptExtension
 
 class AndroidHiltConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
@@ -14,18 +12,14 @@ class AndroidHiltConventionPlugin : Plugin<Project> {
 
             with(pluginManager) {
                 apply("dagger.hilt.android.plugin")
-                apply("org.jetbrains.kotlin.kapt")
-            }
-
-            extensions.configure<KaptExtension> {
-                correctErrorTypes = true
+                apply("com.google.devtools.ksp")
             }
 
             dependencies {
                 "implementation"(libs.findLibrary("hilt.android").get())
-                "kapt"(libs.findLibrary("hilt.compiler").get())
+                "ksp"(libs.findLibrary("hilt.compiler").get())
                 "implementation"(libs.findLibrary("hilt.ext.work").get())
-                "kapt"(libs.findLibrary("hilt.ext.compiler").get())
+                "ksp"(libs.findLibrary("hilt.ext.compiler").get())
             }
         }
     }
