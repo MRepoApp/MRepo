d45fd0e40df2ae0c65a35be7f6e3f5869a227c5a
ya0211 <sanmer@sanmer.dev>
2023-09-25 00:35:22 +0800

    Use `staticCompositionLocalOf`

--- app/src/main/kotlin/com/sanmer/mrepo/ui/providable/LocalSuState.kt
+++ app/src/main/kotlin/com/sanmer/mrepo/ui/providable/LocalSuState.kt
@@ -1,6 +1,6 @@
 package com.sanmer.mrepo.ui.providable
 
-import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.runtime.staticCompositionLocalOf
 import com.sanmer.mrepo.app.Event
 
-val LocalSuState = compositionLocalOf { Event.NON }
+val LocalSuState = staticCompositionLocalOf { Event.NON }
--- app/src/main/kotlin/com/sanmer/mrepo/ui/providable/LocalUserPreferences.kt
+++ app/src/main/kotlin/com/sanmer/mrepo/ui/providable/LocalUserPreferences.kt
@@ -1,6 +1,6 @@
 package com.sanmer.mrepo.ui.providable
 
-import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.runtime.staticCompositionLocalOf
 import com.sanmer.mrepo.datastore.UserPreferencesExt
 
-val LocalUserPreferences = compositionLocalOf { UserPreferencesExt.default() }
+val LocalUserPreferences = staticCompositionLocalOf { UserPreferencesExt.default() }
