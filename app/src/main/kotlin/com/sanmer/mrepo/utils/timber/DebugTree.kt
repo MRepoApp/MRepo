06932c8c41ee673871d08f0abe35a10a3d330a84
SanmerDev <sanmer@sanmer.dev>
2024-02-06 18:22:26 +0800

    Update logging tag

--- app/src/main/kotlin/com/sanmer/mrepo/utils/timber/DebugTree.kt
+++ app/src/main/kotlin/com/sanmer/mrepo/utils/timber/DebugTree.kt
@@ -4,12 +4,7 @@ import timber.log.Timber
 
 class DebugTree : Timber.DebugTree() {
     override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
-        val newTag = tag?.let {
-            val threadName = Thread.currentThread().name
-            return@let "<MR_DEBUG><$threadName>$tag"
-        }
-
-        super.log(priority, newTag, message, t)
+        super.log(priority, "<MR_DEBUG>$tag", message, t)
     }
 
     override fun createStackElementTag(element: StackTraceElement): String {
--- app/src/main/kotlin/com/sanmer/mrepo/utils/timber/ReleaseTree.kt
+++ app/src/main/kotlin/com/sanmer/mrepo/utils/timber/ReleaseTree.kt
@@ -6,8 +6,4 @@ class ReleaseTree : Timber.DebugTree() {
     override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
         super.log(priority, "<MR_REL>$tag", message, t)
     }
-
-    override fun createStackElementTag(element: StackTraceElement): String {
-        return super.createStackElementTag(element) + "(L${element.lineNumber})"
-    }
 }
