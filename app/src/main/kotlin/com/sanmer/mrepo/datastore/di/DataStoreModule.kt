d62863563f465799ebcc17ed5aeddd41561d0854
SanmerDev <sanmer@sanmer.dev>
2024-05-20 01:42:12 +0800

    Remove unused code

--- app/src/main/kotlin/com/sanmer/mrepo/datastore/di/DataStoreModule.kt
+++ app/src/main/kotlin/com/sanmer/mrepo/datastore/di/DataStoreModule.kt
@@ -6,29 +6,24 @@ import androidx.datastore.core.DataStoreFactory
 import androidx.datastore.dataStoreFile
 import com.sanmer.mrepo.datastore.UserPreferences
 import com.sanmer.mrepo.datastore.UserPreferencesSerializer
-import com.sanmer.mrepo.di.ApplicationScope
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
-import kotlinx.coroutines.CoroutineScope
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object DataStoreModule {
-
     @Provides
     @Singleton
     fun providesUserPreferencesDataStore(
         @ApplicationContext context: Context,
-        userPreferencesSerializer: UserPreferencesSerializer,
-        @ApplicationScope applicationScope: CoroutineScope
+        userPreferencesSerializer: UserPreferencesSerializer
     ): DataStore<UserPreferences> =
         DataStoreFactory.create(
-            serializer = userPreferencesSerializer,
-            scope = applicationScope,
+            serializer = userPreferencesSerializer
         ) {
             context.dataStoreFile("user_preferences.pb")
         }
