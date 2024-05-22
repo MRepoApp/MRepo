e3066866d464a6f227c7a5e975e4872e809bd447
SanmerDev <sanmer@sanmer.dev>
2024-05-22 16:29:07 +0800

    Optimize `datastore`

--- app/src/main/kotlin/com/sanmer/mrepo/datastore/UserPreferencesSerializer.kt
+++ app/src/main/kotlin/com/sanmer/mrepo/datastore/UserPreferencesSerializer.kt
@@ -7,17 +7,17 @@ import java.io.InputStream
 import java.io.OutputStream
 import javax.inject.Inject
 
-class UserPreferencesSerializer @Inject constructor() : Serializer<UserPreferences> {
-    override val defaultValue: UserPreferences = UserPreferencesCompat.default().toProto()
+class UserPreferencesSerializer @Inject constructor() : Serializer<UserPreferencesCompat> {
+    override val defaultValue = UserPreferencesCompat.default()
 
-    override suspend fun readFrom(input: InputStream): UserPreferences =
+    override suspend fun readFrom(input: InputStream) =
         try {
-            UserPreferences.parseFrom(input)
-        } catch (exception: InvalidProtocolBufferException) {
-            throw CorruptionException("cannot read proto", exception)
+            UserPreferences.parseFrom(input).let(::UserPreferencesCompat)
+        } catch (e: InvalidProtocolBufferException) {
+            throw CorruptionException("cannot read proto", e)
         }
 
-    override suspend fun writeTo(t: UserPreferences, output: OutputStream) {
-        t.writeTo(output)
+    override suspend fun writeTo(t: UserPreferencesCompat, output: OutputStream) {
+        t.toProto().writeTo(output)
     }
 }
--- app/src/main/kotlin/com/sanmer/mrepo/datastore/di/DataStoreModule.kt
+++ app/src/main/kotlin/com/sanmer/mrepo/datastore/di/DataStoreModule.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.core.DataStoreFactory
 import androidx.datastore.dataStoreFile
-import com.sanmer.mrepo.datastore.UserPreferences
+import com.sanmer.mrepo.datastore.UserPreferencesCompat
 import com.sanmer.mrepo.datastore.UserPreferencesSerializer
 import dagger.Module
 import dagger.Provides
@@ -21,7 +21,7 @@ object DataStoreModule {
     fun providesUserPreferencesDataStore(
         @ApplicationContext context: Context,
         userPreferencesSerializer: UserPreferencesSerializer
-    ): DataStore<UserPreferences> =
+    ): DataStore<UserPreferencesCompat> =
         DataStoreFactory.create(
             serializer = userPreferencesSerializer
         ) {
