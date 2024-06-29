5ebe3d0f2319181e48eeacb29f405f043810eaa1
SanmerDev <sanmer@sanmer.dev>
2024-06-29 14:50:26 +0800

    Optimize `Shell`

--- core/src/main/kotlin/dev/sanmer/mrepo/impl/Shell.kt
+++ core/src/main/kotlin/dev/sanmer/mrepo/impl/Shell.kt
@@ -5,36 +5,30 @@ import android.util.Log
 internal object Shell {
     private const val TAG = "Shell"
 
-    fun String.exec(): Result<String> = try {
-        Log.d(TAG, "exec: $this")
-        val process = ProcessBuilder("sh", "-c", this).start()
-        val output = process.inputStream.bufferedReader().readText()
-            .removeSurrounding("", "\n")
+    fun String.exec(): Result<String> =
+        runCatching {
+            Log.d(TAG, "exec: $this")
+            val process = ProcessBuilder("sh", "-c", this).start()
+            val output = process.inputStream.bufferedReader().readText()
+                .removeSurrounding("", "\n")
 
-        val error = process.errorStream.bufferedReader().readText()
-            .removeSurrounding("", "\n")
+            val error = process.errorStream.bufferedReader().readText()
+                .removeSurrounding("", "\n")
 
-        when {
-            process.waitFor().ok() -> {
-                Log.d(TAG, "output: $output")
-                Result.success(output)
-            }
-            else -> {
-                Log.d(TAG, "error: $error")
-                Result.failure(RuntimeException(error))
-            }
+            require(process.waitFor().ok()) { error }
+            Log.d(TAG, "output: $output")
+
+            output
+        }.onFailure {
+            Log.e(TAG, Log.getStackTraceString(it))
         }
-    } catch (e: Throwable) {
-        Log.e(TAG, Log.getStackTraceString(e))
-        Result.failure(e)
-    }
 
     fun String.exec(
         stdout: (String) -> Unit,
         stderr: (String) -> Unit
-    ) = try {
-        Log.d(TAG, "submit: ${this@exec}")
-        val process = ProcessBuilder("sh", "-c", this@exec).start()
+    ) = runCatching {
+        Log.d(TAG, "exec: $this")
+        val process = ProcessBuilder("sh", "-c", this).start()
         val output = process.inputStream.bufferedReader()
         val error = process.errorStream.bufferedReader()
 
@@ -44,17 +38,13 @@ internal object Shell {
         }
 
         error.forEachLine {
-            Log.d(TAG, "stderr: $it")
+            Log.d(TAG, "error: $it")
             stderr(it)
         }
 
-        when {
-            process.waitFor().ok() -> Result.success(true)
-            else -> Result.failure(RuntimeException())
-        }
-    } catch (e: Throwable) {
-        Log.e(TAG, Log.getStackTraceString(e))
-        Result.failure(e)
+        require(process.waitFor().ok())
+    }.onFailure {
+        Log.e(TAG, Log.getStackTraceString(it))
     }
 
     private fun Int.ok() = this == 0
